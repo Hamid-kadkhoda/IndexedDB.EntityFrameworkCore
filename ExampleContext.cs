7ce9ebc25fb6662f5ec3ad0c68596d6a93f2aa42
Hamid kadkhoda <hamid.kadkhoda@protonmail.com>
2025-10-26 00:49:23 +0330

    add example

---
+++ ExampleContext.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Microsoft.JSInterop;
+
+namespace IndexedDB.EntityFrameworkCore;
+
+internal class Example
+{
+    public Guid Id { get; set; }
+
+    public string? Name { get; set; }
+}
+
+internal class ExampleContext : IndexedDbContext
+{
+    public ExampleContext(IJSRuntime jsRuntime, 
+        string databaseName = "ExampleDB", 
+        int version = 1)
+        : base(jsRuntime, databaseName, version)
+    {
+
+    }
+
+    public IndexedDbSet<Example>? Examples { get; set; }
+}
